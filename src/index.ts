621d9df27424b909b4216432b92521081b157530
lwz <liangweizhu@siteshun.com>
2024-12-05 15:18:24 +0800

    fix: async

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
+'use strict';
+
 interface PromiseConstructor {
     try: <T>(fn: (...args: any[]) => T, ...args: any[]) => Promise<T>;
 }
 
 Promise.try || (Promise.try = function (func, ...args) {
-    if(new.target) {
+    if (new.target) {
         throw new TypeError("Promise.try is not a constructor");
     }
-    return new this((resolve, reject) => {
+    return new this(async (resolve, reject) => {
         try {
-            resolve(func.apply(this, args));
+            resolve(await func.apply(this, args));
         } catch (error) {
             reject(error);
         }
